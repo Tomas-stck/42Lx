dbe24cb46a75335e4a32ab47b7bd482f4b0629d7
Tomas Maria Otto Pinto Coelho Stocker <totto-pi@c1r17s3.42lisboa.com>
2022-10-31 12:03:51 +0000

    42 add

---
+++ ft_isalpha.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: totto-pi <totto-pi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 14:15:16 by totto-pi          #+#    #+#             */
+/*   Updated: 2022/10/25 17:16:32 by totto-pi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+// if TRUE return 1
+//if FALSE return 0
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
+int	main(void)
+{
+	char	a = 'a';
+
+	if (ft_isalpha(a))
+		printf("True");
+	else
+		printf("False");
+	return (0);
+}
