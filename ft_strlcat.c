aba0238212b19776dcbe25c625c97c3ef7c0b627
Tomas Maria Otto Pinto Coelho Stocker <totto-pi@c1r16s4.42lisboa.com>
2022-10-31 17:13:52 +0000

    refresh

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: totto-pi <totto-pi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/31 12:38:15 by totto-pi          #+#    #+#             */
-/*   Updated: 2022/10/31 16:10:57 by totto-pi         ###   ########.fr       */
+/*   Created: 2022/10/31 15:33:15 by totto-pi          #+#    #+#             */
+/*   Updated: 2022/10/31 16:51:17 by totto-pi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,52 @@
 #include <stdlib.h>
 #include "ft_strlen.c"
 
-//The strlcpy() function copies up to size-1 characters from the NUL-terminated string src to dst, NUL-terminating the result.
 //use `pkg-config --cflags --libs libbsd-overlay` after gcc instructions
 
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	int		len;
-
-	len = ft_strlen(src);
-	if (!(size > 0) && (src[len] != 0))
+	size_t	slen;
+	size_t	dlen;
+	
+	slen = ft_strlen(src);
+	if ((size <= 0) && (src[slen + 1] != 0))
 		return (0);
+	dlen = ft_strlen(dst);
 	i = 0;
-	while (i < size - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
+	while (i < size -1)
+		dst[dlen++] = src[i++];
 	dst[i] = 0;
-	return (size);
+	return (i + slen - 1);
 }
 
+/*size_t	i;
+	char	temp;
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (!(size > 0) && (src[len + 1] != '\0') & (dst[size] != '\0'))
+		return (0);
+	i = 0;
+	if (ft_strlen(dst) < ft_strlen(src))
+	{
+		while (i < size)
+		{
+		temp = src[i];
+		dst[i] = temp;
+		i++;
+		}
+	}
+	
+	dst[i] = '\0';
+	return (size);*/
+	
 int main()
 {
 	char frase[] = "Life is an amazing ride, even when it shits on your dreams.";
 	char dest[22] = "Ola ";
 	printf("%s\n", frase );
-	ft_strlcpy(dest, frase, 7);
+	strlcat(dest, frase, 7);
 	printf("%s\n", dest );
 	return (0);
 }
