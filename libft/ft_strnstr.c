e323af16a18614eb2c24616d44b84794b70754d2
Tomas Maria Otto Pinto Coelho Stocker <totto-pi@c1r14s5.42lisboa.com>
2022-11-09 18:57:28 +0000

    refresh

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,52 +6,28 @@
 /*   By: totto-pi <totto-pi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:48:01 by totto-pi          #+#    #+#             */
-/*   Updated: 2022/11/09 18:27:30 by totto-pi         ###   ########.fr       */
+/*   Updated: 2022/11/09 18:55:20 by totto-pi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char	*ft_strnstr(const char *big, const char *little, size_t len)
-// {
-// 	size_t	l;
-// 	size_t	j;
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	l;
 
-// 	l = ft_strlen(little);
-// 	if (*little == '\0')
-// 		return ((char *)big);
-// 	while (*big && l <= len)
-// 	{
-// 		j = 0;
-// 		if (*big == *little)
-// 		{
-// 			while (*big == *little)
-// 			{
-				
-// 				if ( *little == '\0')
-// 					return ((char *)little);
-// 			}
-// 		}
-// 		else
-// 			big++;
-// 	}
-// 	if ( l == len)
-// 		return (NULL);
-// }
-
-// char	*ft_strnstr(const char *big, const char *little, size_t len)
-// {
-// 	size_t	n;
-
-// 	if (*little == 0)
-// 		return ((char *)big);
-// 	n = ft_strlen(little);
-// 	while (*big && n <= len)
-// 	{
-// 		if (*big == *little && ft_strncmp(big, little, n) == 0)
-// 			return ((char *)big);
-// 		++big;
-// 		--len;
-// 	}
-// 	return (NULL);
-// }
+	l = ft_strlen(little);
+	if (*little == '\0' || len == 0)
+		return ((char *)big);
+	while (*big && l <= len)
+	{
+		if (*big == *little && ft_strncmp( big, little, l) == 0)
+			return ((char *)big);
+		else
+		{
+			big++;
+			len--;
+		}
+	}
+	return (NULL);
+}
